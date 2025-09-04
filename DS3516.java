75fd483be0f67a7a91cabdb771896b45be0a8bdd
Gorish Gupta <gorishgupta50@gmail.com>
2025-09-04 21:32:39 +0530

    Update DS3516.java

---
+++ DS3516.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        int a = Math.abs(z - x);
+        int b = Math.abs(z - y);
+        if (a == b) {
+            return 0;
+        }
+        if (a < b)
+            return 1;
+        else
+            return 2;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.findClosest(1, 2, 3)); // Output: 2
+        System.out.println(sol.findClosest(1, 3, 2)); // Output: 0
+        System.out.println(sol.findClosest(2, 1, 3)); // Output: 1
+    }
+}

455885cbd85d4b113fce53e1325cc019c7b31187
Gorish Gupta <104247577+GorishGupta@users.noreply.github.com>
2025-09-13 23:55:04 +0530

    Create DS3541.java

---
+++ DS3541.java
@@ -0,0 +1,21 @@
+public class DS3541 {
+        public int maxFreqSum(String s) {
+        int[] freq = new int[26];
+        int maxVowel = 0, maxConso = 0;
+        for (char c : s.toCharArray()) {
+            int i = c - 'a';
+            freq[i]++;
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                maxVowel = Math.max(maxVowel, freq[i]);
+            else
+                maxConso = Math.max(maxConso, freq[i]);
+        }
+        return maxVowel + maxConso;
+    }
+    public static void main(String[] args) {
+        DS3541 obj = new DS3541();
+        String s = "successes";
+        System.out.println(obj.maxFreqSum(s));
+    }
+}
+
